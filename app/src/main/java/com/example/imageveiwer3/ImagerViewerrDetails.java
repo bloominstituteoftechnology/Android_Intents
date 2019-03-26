9ffdd567cfed1c2291c07092273ddb0b5d197dc1
AlhousseineC <Alhoussein.camara@gmail.com>
2019-03-25 23:38:23 -0500

    Create an Intent and onActiviatyResults()

--- app/src/main/java/com/example/imageveiwer3/ImagerViewerrDetails.java
+++ app/src/main/java/com/example/imageveiwer3/ImagerViewerrDetails.java
@@ -1,7 +1,7 @@
 package com.example.imageveiwer3;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class ImagerViewerrDetails extends AppCompatActivity {
 
