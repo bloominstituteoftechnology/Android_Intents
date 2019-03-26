3f0fd345898767c61e2fb0a7e2d6c24c9f19fb3a
AlhousseineC <Alhoussein.camara@gmail.com>
2019-03-26 00:44:01 -0500

    getIntent and finish

--- app/src/main/java/com/example/imageveiwer3/ImageViewerDetails.java
+++ app/src/main/java/com/example/imageveiwer3/ImageViewerDetails.java
@@ -8,19 +8,21 @@ package com.example.imageveiwer3;
 
 public class ImageViewerDetails extends AppCompatActivity {
 
-    TextView imageDetails;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_viewer_details);
 
-        imageDetails = findViewById(R.id.details_of_image);
+        TextView imageDetails = findViewById(R.id.details_of_image);
         ImageView receivedImage = findViewById(R.id.received_image);
 
         Intent getImage = getIntent();
         ImageViewerModel imageViewerModel = (ImageViewerModel) getImage.getSerializableExtra("sentImage");
         receivedImage.setImageURI(imageViewerModel.getPictureUri());
+
+        imageDetails.setText(imageViewerModel.getName());
 
 
 
