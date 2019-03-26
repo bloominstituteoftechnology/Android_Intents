dda87de72054930416ee68716d1e07d0c450c87a
AlhousseineC <Alhoussein.camara@gmail.com>
2019-03-25 20:43:17 -0500

    Create generateTextView method and ImageViewerDetails()

---
+++ app/src/main/java/com/example/imageveiwer3/ImagerViewerrDetails.java
@@ -0,0 +1,13 @@
+package com.example.imageveiwer3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagerViewerrDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imager_viewerr_details);
+    }
+}
